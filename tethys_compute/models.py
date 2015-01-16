36f530e139220369d868fd9d346cfa889077b074
Scott <sdc50@byu.net>
2015-01-16 10:37:04 -0700

    implemented a python api interface for starcluster

--- tethys_compute/models.py
+++ tethys_compute/models.py
@@ -1,4 +1,50 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
 class TethysJob(models.Model):
-    pass
+    STATUSES = (
+        ('PEN', 'Pending'),
+        ('SUB', 'Submitted'),
+        ('RUN', 'Running'),
+        ('COM', 'Complete'),
+        ('ERR', 'Error'),
+    )
+    name = models.CharField(max_length=30)
+    user = models.ForeignKey(User)
+    group = models.CharField(max_length=30)
+    creation_time = models.DateTimeField(default=timezone.now())
+    submission_time = models.DateTimeField()
+    completion_time = models.DateTimeField()
+    status = models.CharField(max_length=3, choices=STATUSES)
+
+    def execute(self):
+        """
+
+        :return:
+        """
+        pass
+
+    def stop(self):
+        """
+
+        :return:
+        """
+        pass
+
+    def pause(self):
+        """
+
+        :return:
+        """
+        pass
+
+class CondorJob(TethysJob):
+    scheduler = models.CharField(max_length=12)
+    ami = models.CharField(max_length=9)
+
+    @property
+    def condorpy_job(self):
+        pass
--- tethys_compute/views.py
+++ tethys_compute/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from starcluster_api import StarCluster
+
+from tethys_compute.models import TethysJob
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the compute index.")
+
+def create_cluster(request):
+    sc = StarCluster()
+
+
+
